238ca928d61b03a82d90523eba7dcaae83344b29
Dee89 <dorisbaruti@yahoo.com>
2026-03-06 11:32:57 +0000

    Added Spring Boot backend project

---
+++ src/main/java/com/doris/agenticcrud/AgenticcrudApplication.java
@@ -0,0 +1,13 @@
+package com.doris.agenticcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doris/agenticcrud/AgenticcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doris.agenticcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenticcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
